db95d199263db084778068de60d1cd9cfb783f1f
almostprogramer1 <zxcsdfrty96@gmail.com>
2023-07-22 19:27:02 +0200

    2023_07_22 first commit

---
+++ dev_app/src/main/java/com/dev_app/dev_app/DevAppApplication.java
@@ -0,0 +1,13 @@
+package com.dev_app.dev_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevAppApplication.class, args);
+	}
+
+}
