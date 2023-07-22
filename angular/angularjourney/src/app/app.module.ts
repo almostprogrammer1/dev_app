8d2db963589ff0d516cb0e810a98fd02b31e29e1
MisiekEr <kuczynski.michal92@gmail.com>
2023-07-23 01:44:32 +0200

    2023_07_23- initial commit z login pagem.

---
+++ angular/angularjourney/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+import { AppComponent } from './app.component';
+import { LoginFormComponent } from './login-form/login-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
